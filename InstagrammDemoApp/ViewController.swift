6480d2ebb4c048369e152f67538ee3a90c6cf7c1
Daniil Vereschagin <vereshhagin98@mail.ru>
2019-01-12 13:55:13 +0300

    Initial Commit

---
+++ InstagrammDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagrammDemoApp
+//
+//  Created by Daniil Vereschagin on 1/12/19.
+//  Copyright © 2019 Daniil Vereschagin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
